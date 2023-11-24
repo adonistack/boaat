0ef711387a30772758c91f4dbc0920806475b0e9
Adonis stack <69584141+adonistack@users.noreply.github.com>
2023-11-24 22:57:25 +0200

    Add files via upload

---
+++ boaat/data/locations.js
@@ -0,0 +1,8 @@
+export const locations = [
+    { image: "access/location1.jpg", address: `עולי הגרדום 22 ת"א` },
+    { image: "access/location2.jpg", address: "האלוף דוד 6 ראשון לציון" },
+    { image: "access/location3.jpg", address: `הרוגי המלכות 10 ת"א` },
+    { image: "access/location4.jpg", address: "רחוב האנפה 6 טירת הכרמל" },
+    { image: "access/location5.jpg", address: "הברוש 8 באר יעקב" }
+  ];
+  
---
+++ boaat/data/logos.js
@@ -0,0 +1,10 @@
+export const logos = [
+    { src: "access/logo1.jpg", alt: "רם אדום" },
+    { src: "access/logo2.jpg", alt: "ברומי פרוקים" },
+    { src: "access/logo3.jpg", alt: "א.ח.ס כוח אדם ובניה" },
+    { src: "access/logo4.jpg", alt: "פדלין לבניה" },
+    { src: "access/logo5.jpg", alt: "רוטשטיין" },
+    { src: "access/logo6.jpg", alt: "שטרן" },
+    { src: "access/logo7.jpg", alt: "צרפתי צבי ובניו" }
+  ];
+  
